4ef55868d57066ce3800658be025fb6e6c9f1e02
Raineri02 <asa.raineri20@gmail.com>
2025-10-10 19:47:41 -0300

    Ajustando o front para relacionarcom o back

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -14,6 +14,7 @@ import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Navbar } from "@/components/Navbar";
 import { useToast } from "@/hooks/use-toast";
+import { api } from "@/lib/api"; 
 
 /*
   Melhorias realizadas:
