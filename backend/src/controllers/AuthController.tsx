6e42f872e19580af844120e2b9e4f82e444962f9
Raineri02 <asa.raineri20@gmail.com>
2025-10-10 22:24:54 -0300

    01

--- backend/src/controllers/AuthController.tsx
+++ backend/src/controllers/AuthController.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect, ReactNode } from "react";
-import { api } from "../api/api";
+import { api } from "../api/api.ts";
 
 interface User {
   id: number;
--- src/apiClient.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Configura uma instância base do Axios
-const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api", // URL base do backend
-  withCredentials: true, // Permitir envio de cookies, se necessário
-});
-
-export default apiClient;
